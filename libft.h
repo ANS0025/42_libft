d1bcc8fd5534cb0e841a8ec6266d92526bf16775
ANS0025 <niko.aegis@gmail.com>
2023-11-04 21:38:13 +0900

    add: ft_printf

--- libft.h
+++ libft.h
@@ -16,6 +16,7 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <limits.h>
+# include <stdarg.h>
 
 int				ft_isalpha(int c);
 int				ft_isdigit(int c);
@@ -52,5 +53,14 @@ void			ft_putchar_fd(char c, int fd);
 void			ft_putstr_fd(char *s, int fd);
 void			ft_putendl_fd(char *s, int fd);
 void			ft_putnbr_fd(int n, int fd);
+int				ft_printf(const char *format, ...);
+int				ft_parse(char c, va_list args);
+int				ft_putchar(char c);
+int				ft_putstr(char *s);
+int				ft_putptr(void *ptr);
+int				ft_putnbr(int n);
+int				ft_putunsigned(unsigned int n);
+int				ft_puthex(unsigned long long n);
+int				ft_puthexupper(unsigned long long n);
 
 #endif
